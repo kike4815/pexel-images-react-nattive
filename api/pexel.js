e377bba10e71cff157adb1ad9134eb7b242621de
kike4815 <e.pedros.sanchez@gmail.com>
2021-09-13 19:18:29 +0200

    create Screens / components & api call

---
+++ api/pexel.js
@@ -0,0 +1,10 @@
+// "Authorization: 563492ad6f91700001000001a50a88b169414f23997c51b91ce7e946" \
+//"https://api.pexels.com/v1/search?query=people"
+import axios from "axios";
+
+export const getImages = async (searchTerm = "programming") =>
+  await axios.get(`https://api.pexels.com/v1/search?query=${searchTerm}`, {
+    headers: {
+      Authorization: "563492ad6f91700001000001a50a88b169414f23997c51b91ce7e946",
+    },
+  });
